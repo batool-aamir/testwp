f2054dea6aa7f292a1cfe8846f2c4c34a02734fc
batool-aamir <batoolaamir143@gmail.com>
2020-02-03 08:41:15 +0500

    linking divi-child to css n js files

---
+++ wp-content/themes/divi-child/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+add_action('wp_enqueue_scripts','my_theme_enqueue_styles');
+
+
+function my_theme_enqueue_styles() {
+    wp_enqueue_style('root-style', get_stylesheet_directory_uri() . '/style.css' );
+    wp_enqueue_style('custom-style', get_stylesheet_directory_uri() . '/css/custom.css' );
+    wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/script.js', array(), '1.0.0', true);
+    wp_enqueue_style( 'load-font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+
+ 
+}
+
+
+?>
